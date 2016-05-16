21cd1cbf5ffdd8f5c453768638cc8ecdaf079566
Denys Shabalin <denys.shabalin@epfl.ch>
2016-05-15 20:45:00 -0400

    Initial support for targets in compiler

--- tools/src/main/scala/scala/scalanative/compiler/Compiler.scala
+++ tools/src/main/scala/scala/scalanative/compiler/Compiler.scala
@@ -41,7 +41,7 @@ final class Compiler(opts: Opts) {
   }
 
   private def codegen(assembly: Seq[Defn]): Unit = {
-    val gen = new GenTextualLLVM(assembly)
+    val gen = new GenTextualLLVM(opts.target, assembly)
     serializeFile((defns, bb) => gen.gen(bb), assembly, opts.outpath)
   }
 
